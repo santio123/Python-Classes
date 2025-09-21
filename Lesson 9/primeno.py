bc149ce0cd7bac5cfca9d44a3a0c30cf653278e2
santio123 <santioflorin1007@gmail.com>
2025-09-21 12:40:48 +0530

    primenumber

---
+++ Lesson 9/primeno.py
@@ -0,0 +1,11 @@
+# prime number generator
+l = int(input("Enter the number 1 : "))
+u = int(input("Enter the number 2 : "))
+
+for i in range(l,u+1):
+
+    for j in (2.i):
+        if i%j == 0:
+            break
+    else:
+        print(i)
