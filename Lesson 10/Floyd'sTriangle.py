d3b7d8aa441ab428b4d4d2afbcf96c24a5352662
santio123 <santioflorin1007@gmail.com>
2025-09-27 12:37:14 +0530

    Floyd's triangle

---
+++ Lesson 10/Floyd'sTriangle.py
@@ -0,0 +1,9 @@
+# Floyd's Triangle
+
+n = int(input("enter the number of rows : "))
+k = 1
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(k, end = " ")
+        k = k + 1
+    print()
