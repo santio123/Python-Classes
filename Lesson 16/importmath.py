da524331cc44052bdf7aa93496129157f8c759f2
santio123 <santioflorin1007@gmail.com>
2025-10-18 12:54:02 +0530

    math

--- Lesson 16/importmath.py
+++ Lesson 16/importmath.py
@@ -4,7 +4,7 @@ import math
 print("the ceil value of 89.6 : ",math.ceil(89.6))
 print("the floor value of 89.6 : ",math.floor(89.6))
 print("the absolute of -87 : ",math.fabs(-87))
-print("the ceil value of 24,26 : ",math.gcd(24,26))
+print("the gcd value of 24,26 : ",math.gcd(24,26))
 
 x = 89
 y = -79
