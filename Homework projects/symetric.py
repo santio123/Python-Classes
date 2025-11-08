4fa55fd82fae04092f27d6d4994472c179840a22
santio123 <santioflorin1007@gmail.com>
2025-11-08 12:58:01 +0530

    weather

---
+++ Homework projects/symetric.py
@@ -0,0 +1,12 @@
+# symetric difference
+
+setx = {'green','orange','blue'}
+sety = {'orange','blue','red'}
+print(setx)
+print(sety)
+print("the intersection of two sets are")
+print(setx.intersection(sety))
+print("the union of two sets are")
+print(setx.union(sety))
+print("the symetricdifference of two sets are")
+print(setx.symetricdifference(sety))
--- Lesson 17/weather.py
+++ Lesson 17/weather.py
@@ -1,8 +1,14 @@
 # weather condition
-
-weather = (0,1,1,1,0,0,1)
-
+weather = (1,0,0,0,1,1,0,1,1)
 rainy = 0
 sunny = 0
-
-for i in range(0,7)
+for i in weather:
+    if(i==0):
+        rainy+=1
+    else:
+        sunny+=1
+    
+if(sunny>rainy):
+    print("Good weather")
+else:
+    print("Bad weather")
