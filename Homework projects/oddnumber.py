ef5759802b27d1232bf6b2e4679ad5516d7aa504
santio123 <santioflorin1007@gmail.com>
2025-11-09 12:58:20 +0530

    oddnumber

---
+++ Homework projects/oddnumber.py
@@ -0,0 +1,9 @@
+# find the odd number
+
+list1 = [1,3,23,34,13,12,8,9]
+list2 = [i for i in list1 if i%2 ==1]
+print(list2)
+
+list3 = ['apple','banana','pineapple']
+list4 = [i.capitalize() for i in list3]
+print(list4)
