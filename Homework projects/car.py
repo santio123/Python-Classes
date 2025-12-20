b83f1ec1b9b47d940066bc7b6170176110d1ca05
santio123 <santioflorin1007@gmail.com>
2025-12-20 09:29:03 +0530

    hi

---
+++ Homework projects/car.py
@@ -0,0 +1,23 @@
+# polymorphism
+
+class BMW:
+    def name(self):
+        print("BMW")
+    
+    def typeofcar(self):
+        print("Normal car")
+
+
+class Ferrari:
+    def name(self):
+        print("Ferrari")
+    
+    def Typeofcar(self):
+        print("Race car")
+
+
+obj1 = BMW()
+obj2 = Ferrari()
+for car in (obj1,obj2):
+car.name()
+car.Typeofcar()
