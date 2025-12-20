09b4e69d0100e1c3294deb53748155d68e63d6ab
santio123 <santioflorin1007@gmail.com>
2025-12-20 12:58:02 +0530

    hi

---
+++ Lesson 29/collision.py
@@ -0,0 +1,52 @@
+import pygame
+
+pygame.init()
+SWIDTH = 500
+SHEIGHT = 400
+screen = pygame.display.set_mode((SWIDTH,SHEIGHT))
+mspeed = 1
+
+font = pygame.font.SysFont("Times New Roman",72)
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self,color,h,w):
+        super().__init__()
+        self.image = pygame.Surface([w,h])
+        self.rect = self.image.get_rect()
+    def move(self,xchange,ychange):
+        self.rect.x = max(min(self.rect.x +xchange,SWIDTH -self.rect.w),0)
+        self.rect.y = max(min(self.rect.y + ychange,SHEIGHT -self.rect.h),0)
+clock =pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+sprite1 = Sprite("green",20,20)
+sprite1.rect.x = 200
+sprite1.rect.y = 200
+all_sprites.add(sprite1)
+
+sprite2 = Sprite("green",20,20)
+sprite2.rect.x = 100
+sprite2.rect.y = 50
+all_sprites.add(sprite2)
+
+while True:
+    screen.fill("black")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+    keys = pygame.key.get_pressed()
+
+    xchange = (keys[pygame.K_RIGHT]- keys[pygame.K_LEFT]) *mspeed
+    ychange = (keys[pygame.K_DOWN]- keys[pygame.K_UP]) *mspeed
+    if sprite1.rect.colliderect(sprite2.rect):
+
+        win_text = font.render("You Win!!", True, pygame.color('yellow'))
+        screen.blitz(win_text, (200,200))
+        all_sprites.remove(sprite2)
+
+    sprite1.move(xchange,ychange)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(90)
+    
+
+        
