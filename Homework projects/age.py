1848ac66567a44d4f238dab78ec5e37dad13c366
santio123 <santioflorin1007@gmail.com>
2026-01-10 12:10:58 +0530

    age

---
+++ Homework projects/age.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from datetime import date
+bd = int(input("enter your birthdate year : "))
+year = 2025 - bd
+root = Tk()
+root.title("Age calculater")
+root.geometry("400x500")
+root.configure(bg = 'orange')
+g = ("your age is "year)
+frame1 = Frame(root,bg = 'darkorange',bd = 3,width = 450,height = 300,relief = 'sunken')
+frame1.place(x=0,y=0)
+label1 = Label(frame1,bg ='darkorange',text = 'Name')
+label1.place(x=20,y=20)
+label2 = Label(frame1,bg = 'darkorange',text = 'Password')
+label2.place(x=20,y=80)
+entry1 = Entry(frame1)
+entry1.place(x=120,y=20)
+entry2 = Entry(frame1)
+entry2.place(x=120,y=80)
+button1 = Button(frame1,bg = 'black',fg = 'darkorange',text = 'Login',command = login)
+button1.place(x=70,y=120)
+text1 = Text(root, bg = 'beige',width = 400,height = 8,bd = 3,relief = 'sunken')
+text1.place(x=0,y=170)
+
+root.mainloop()
