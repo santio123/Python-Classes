e192bba2e4d24c2d0afbf42571ca2e9b3099f400
santio123 <santioflorin1007@gmail.com>
2026-03-01 16:16:52 +0530

    hi

---
+++ Lesson 38/asymptoticnotation.py
@@ -0,0 +1,15 @@
+def printnumber(n):
+    iteration = 0
+    
+    print("The number entered by user is:", n)
+    
+    iteration += 1
+    
+    print("Total iterations done by the code is:", iteration, "\n")
+
+
+printnumber(10)
+printnumber(20)
+
+print("\nWith any 'n', the time taken by our code won't change")
+
---
+++ Lesson 38/asymtotic2.py
@@ -0,0 +1,11 @@
+def OnTime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iterations+=1
+    print("When n is ",n,"Iterations = ",iteration)
+
+OnTime(10)
+OnTime(20)
+OnTime(42)
+
+print("\nwith every 'n' the last time taken and iterations will increase")
