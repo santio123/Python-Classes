c9725d376820dbb476f5d8573fc1432e04d22ff4
santio123 <santioflorin1007@gmail.com>
2026-03-28 16:16:21 +0530

    twoodd

--- Lesson 44/oddocurring.py
+++ Lesson 44/oddocurring.py
@@ -1,11 +1,12 @@
 def OddOccurring(arr):
  res=0
  for element in arr:
-  res=res^element
- return res
+    res=res^element
+    return res
 arr=[]
 n=int(input("Enter array size : "))
 while(n):
- num=int(input("Enter number : "))
- arr.append(num)
- n=n-1
+    num=int(input("Enter number : "))
+    arr.append(num)
+    n-=1
+print("\n\nOdd occurring number is :",OddOccurring)
