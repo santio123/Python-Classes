ab83fb992b5a98805a4eabae88c38fea243d3693
santio123 <santioflorin1007@gmail.com>
2026-05-02 16:01:25 +0530

    sqllite

---
+++ Lesson 54/SQLlite.py
@@ -0,0 +1,16 @@
+print("What is the type of above given database -\n 1)Relational Database \n 2)Non-Relational Database")
+answer = int(input("Enter your guess here..."))
+if answer == 2:
+    print("You guessed it right!")
+else:
+    print("Unfortunateley your guess was wrong.")
+print("\nPlease tell your mentor why you guessed this?")
+
+print("What is the type of above given database -\n 1)Relational Database \n 2)Non-Relational Database")
+answer = int(input("Enter your guess here..."))
+if answer == 1:
+    print("You guessed it right!")
+else:
+    print("Unfortunateley your guess was wrong.")
+
+print("\n Please tell your mentor why you guessed this?")
